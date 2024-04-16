eae816cef9305a451c45a28bf1967878973dcf4b
mohammadtonor <mdtonor@gmail.com>
2024-04-16 22:46:28 +0330

    fix

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 echo "Running composer"
 cp /etc/secrets/.env .env
-composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
 
 echo "Clearing caches..."
 php artisan optimize:clear
@@ -12,6 +10,9 @@ php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
+
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
 
 echo "Running migrations..."
 php artisan migrate --force
