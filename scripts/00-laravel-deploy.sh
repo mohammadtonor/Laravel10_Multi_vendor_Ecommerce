9f1e918dd001e4115f6251d2d5e5bbc7ad5bb68e
mohammadtonor <mdtonor@gmail.com>
2024-04-18 04:32:02 +0330

    edit seed

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,12 +2,13 @@
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
+echo "Running migrations..."
+php artisan migrate --force
+php artisan db:seed --force
+
 echo "Caching config..."
 php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running migrations..."
-php artisan migrate --force
-php artisan db:seed 
