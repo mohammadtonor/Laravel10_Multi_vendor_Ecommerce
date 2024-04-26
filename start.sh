7cc8c34f4349c86ac40d51e576a728812f3ac8ad
mohammadtonor <mdtonor@gmail.com>
2024-04-26 03:30:15 +0330

    fix script

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer install --no-dev --working-dir=/var/www/html
+composer dump-autoload
+composer update --no-scripts
 
 echo "Running migrations..."
 php artisan migrate --force
